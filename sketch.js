db0180b3898bafe03bca0927b47245ebd06e6adc
nkdd <65080785+nkdd@users.noreply.github.com>
2020-06-25 21:28:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var canvas,backgroundImage;
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+var allPlayers;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+
+    game = new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    background("white");
+    if(playerCount === 4){
+        game.update(1);
+
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
+
